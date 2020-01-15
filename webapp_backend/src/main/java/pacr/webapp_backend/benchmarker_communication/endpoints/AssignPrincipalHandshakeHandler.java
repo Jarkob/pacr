1d2c67f4cb4a4c39f9f9669c469fc91b8b72d42d
Daniel Augustin <daniel.augustin@student.kit.edu>
2020-01-15 16:29:42 +0100

    Websocket configuration

---
+++ webapp_backend/src/main/java/pacr/webapp_backend/benchmarker_communication/endpoints/AssignPrincipalHandshakeHandler.java
@@ -0,0 +1,53 @@
+package pacr.webapp_backend.benchmarker_communication.endpoints;
+
+import java.security.Principal;
+import java.util.Map;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
+
+/**
+ * This class assigns a name to each new client that is registered.
+ * If the client is already registered it gets its old name reassigned.
+ */
+public class AssignPrincipalHandshakeHandler extends DefaultHandshakeHandler {
+
+    private static final String ATTR_PRINCIPAL = "__principal__";
+
+    private static final String CLIENT_PREFIX = "client";
+    private static int clientID = 0;
+
+    /**
+     * Checks whether the client already has a name associated and assigns it to the client.
+     * If the client is new, a new unique name is created and assigned.
+     *
+     * @param request the HTTP request.
+     * @param wsHandler the web socket handler.
+     * @param attributes all attributes that are set for the client.
+     *
+     * @return a principal object that carries the name of the client.
+     */
+    @Override
+    protected Principal determineUser(ServerHttpRequest request, WebSocketHandler wsHandler, Map<String, Object> attributes) {
+        final String name;
+
+        if (attributes.containsKey(ATTR_PRINCIPAL)) {
+            name = (String) attributes.get(ATTR_PRINCIPAL);
+        } else {
+            name = generateRandomUsername();
+            attributes.put(ATTR_PRINCIPAL, name);
+        }
+
+        return () -> name;
+    }
+
+    /**
+     * Creates a new unique name based on a prefix and an id that is incremented every
+     * time a new name is created.
+     *
+     * @return a unique name.
+     */
+    private String generateRandomUsername() {
+        return CLIENT_PREFIX + clientID++;
+    }
+}
---
+++ webapp_backend/src/main/java/pacr/webapp_backend/benchmarker_communication/endpoints/WebSocketConfig.java
@@ -0,0 +1,43 @@
+package pacr.webapp_backend.benchmarker_communication.endpoints;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+/**
+ * Configures Websockets for this Spring application.
+ */
+@Configuration
+@EnableScheduling
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    /**
+     * Setup the message broker to be memory based.
+     * /topic is used for a broadcast to all registered clients
+     * /queue is used to send messages to a specific client
+     *
+     * Websocket requests must start with /app.
+     *
+     * @param config the message broker configuration.
+     */
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic/", "/queue/");
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+    /**
+     * Set /connect as the entry point for clients where they are assigned a unique name.
+     *
+     * @param registry the endpoint registry.
+     */
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/connect").setHandshakeHandler(new AssignPrincipalHandshakeHandler());
+    }
+
+}
