21a70bd4cd9884e45a487e5b06a0fc5e685a6b80
Daniel Augustin <daniel.augustin@student.kit.edu>
2020-02-15 19:53:57 +0100

    fixed component display in admin interface

--- frontend/src/app/commit-history-job-queue-wrapper/commit-history-job-queue-wrapper.component.ts
+++ frontend/src/app/commit-history-job-queue-wrapper/commit-history-job-queue-wrapper.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-commit-history-job-queue-wrapper',
@@ -8,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class CommitHistoryJobQueueWrapperComponent implements OnInit {
 
   constructor() { }
+
+  @Input() defaultTabIndex = 0;
 
   ngOnInit() {
   }
