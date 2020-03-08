c506d94d681e88eab67e6d564b1a85afed83a44b
Benedikt Hahn <bhahnskul@gmail.com>
2020-03-09 00:39:10 +0100

    Deleted Dashboards and Leaderboards

--- webapp_backend/src/test/java/pacr/webapp_backend/dashboard_management/QueueDashboardModuleTest.java
+++
@@ -1,9 +0,0 @@
-package pacr.webapp_backend.dashboard_management;
-
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
-public class QueueDashboardModuleTest {
-
-}
